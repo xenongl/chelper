65b0a9f3c8d5ac194dbf5a8f2614c87d3d718e5b
Xenon <hankjwimbleton@ya.ru>
2021-12-16 17:49:51 +0500

    embed image

--- cogs/embed.py
+++ cogs/embed.py
@@ -1,5 +1,6 @@
 import discord
 import config
+import json
 
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions, CheckFailure
@@ -11,8 +12,13 @@ class Embed(commands.Cog):
     
     @commands.command()
     @has_permissions(administrator=True)
-    async def embed(self, ctx, title, text):
+    async def embed(self, ctx, title, text, logo):
         embed = discord.Embed(title=title, description=text, color=0xf2ff42)
+        logo = json.loads(logo.lower())
+        
+        if logo:
+            embed.set_image(url='https://i.ibb.co/zP6kRYm/discord.png')
+            
         Log.send(f'{ctx.message.author} created embed')
         await ctx.message.delete()
         await ctx.send(embed=embed)
